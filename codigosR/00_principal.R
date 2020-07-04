774848e47c29adb239cd58d22d8ddd396aece512
Sergio Díaz Guadarrama <42255491+DiazSergio@users.noreply.github.com>
2020-07-03 20:08:10 -0500

    Update 00_principal.R
    Contiene todas las funcionalidades integradas

--- codigosR/00_principal.R
+++ codigosR/00_principal.R
@@ -8,17 +8,19 @@ library(cluster)
 library(sharpshootR)
 library(sf)
 library(gstat)
-library(stats)
+library(raster)
+library(rgdal)
+library(tmap)
 
-# Cambiar directorio de trabajo
-setwd( "D:/Tesis/Datos")
+# Cambiar directorio de trabajo, donde se encuentran los datos para trabajar
+setwd( "D:/Datos")
 
-#cargar y leer JSON
-json <- "perfilesSISLAC.json"
+#cargar y leer los archivos a utilizar
+aoi <- shapefile("areaEstudioUTM.shp") # Se carga la zona de estudio, shapefile en UTM
+json <- "perfilesSISLAC.json"          # Perfiles en formato JSON
 json <- fromJSON(json, flatten = TRUE)$features
 
 ### OBJETIVO ESPECIFICO 1
-
 # validacion de perfiles con error y su exclusion del archivo JSON
 errores <- f0101_validarErrores(json)
 json <- f0102_removerErrores(json,errores)
@@ -33,11 +35,10 @@ spc2 <- f0201_sliceAdaptado(spc)
 
 
 ### OBJETIVO ESPECIFICO 3
-spc1 <- f0301_sliceAQP(spc)
-agregacion <- f0302_agregacionAQP(spc1) # o puede ser spc2,
-print(agregacion)
+spc1 <- f0301_sliceAQP(spc)             # Segmentacion del spc para COS
+agregacion <- f0302_agregacionAQP(spc1) # O spc2, para los valores ajustados
+f0303_plotearAgregacion(agregacion)     # 
 
 
 ### OBJETIVO ESPECIFICO 4
-interpolacion <- f0401_interpolacionIDW(spc)
-
+interpolacion <- f0401_interpolacionIDW(spc1, aoi) # O spc2, para los valores ajustados
